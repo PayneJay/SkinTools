1b4f6bc829ca1b113b0a92fff476da6b698e492a
leather <pjwei1013@163.com>
2020-09-17 00:09:34 +0800

    add:实现自定义View换肤

---
+++ app/src/main/java/com/leather/skindemo/ui/tab/PlaceholderFragment.java
@@ -0,0 +1,72 @@
+package com.leather.skindemo.ui.tab;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+
+import com.leather.skindemo.R;
+import com.leather.skindemo.skin.utils.SkinResources;
+import com.leather.skindemo.view.BarGraphView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class PlaceholderFragment extends Fragment {
+
+    private static final String ARG_SECTION_NUMBER = "section_number";
+
+    private PageViewModel pageViewModel;
+
+    public static PlaceholderFragment newInstance(int index) {
+        PlaceholderFragment fragment = new PlaceholderFragment();
+        Bundle bundle = new Bundle();
+        bundle.putInt(ARG_SECTION_NUMBER, index);
+        fragment.setArguments(bundle);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        pageViewModel = ViewModelProviders.of(this).get(PageViewModel.class);
+        int index = 1;
+        if (getArguments() != null) {
+            index = getArguments().getInt(ARG_SECTION_NUMBER);
+        }
+        pageViewModel.setIndex(index);
+    }
+
+    @Override
+    public View onCreateView(
+            @NonNull LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_tab, container, false);
+        final TextView textView = root.findViewById(R.id.section_label);
+        pageViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                textView.setText(s);
+            }
+        });
+
+        BarGraphView barGraphView = root.findViewById(R.id.barView);
+        List<BarGraphView.BarGraphInfo> list = new ArrayList<>();
+        for (int i = 0; i < 7; i++) {
+            BarGraphView.BarGraphInfo barGraphInfo = new BarGraphView.BarGraphInfo(SkinResources.getInstance().getColor(R.color.color_ffa59d), "子标题" + i, i + 5);
+            list.add(barGraphInfo);
+        }
+        barGraphView.setList(list);
+        return root;
+    }
+}
---
+++ app/src/main/java/com/leather/skindemo/ui/tab/TabActivity.java
@@ -0,0 +1,23 @@
+package com.leather.skindemo.ui.tab;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import com.google.android.material.tabs.TabLayout;
+import com.leather.skindemo.R;
+
+public class TabActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tab);
+        SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
+        ViewPager viewPager = findViewById(R.id.view_pager);
+        viewPager.setAdapter(sectionsPagerAdapter);
+        TabLayout tabs = findViewById(R.id.tabs);
+        tabs.setupWithViewPager(viewPager);
+    }
+}
